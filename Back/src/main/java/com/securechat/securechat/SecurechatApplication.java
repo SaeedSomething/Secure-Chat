a61d6da4c1bae8a03af5947f086e4700d7968834
Saeed <sarfaraz.stz.ggane@gmail.com>
2024-06-14 03:05:43 +0330

    Spring boot project creation for backend

---
+++ Back/src/main/java/com/securechat/securechat/SecurechatApplication.java
@@ -0,0 +1,13 @@
+package com.securechat.securechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurechatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurechatApplication.class, args);
+	}
+
+}
---
+++ Back/src/test/java/com/securechat/securechat/SecurechatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securechat.securechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurechatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
